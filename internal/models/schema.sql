c85218bdc046cd86597879e4bcf1d4c78adf21b7
EcossV <ecoss@grupoviamar.com>
2026-09-03 13:47:11 -0400

    Mod: user tables has check, users now have unique keys.

--- internal/models/schema.sql
+++ internal/models/schema.sql
@@ -32,39 +32,41 @@ CREATE TABLE menu_items (
 );
 
 CREATE TABLE users_role(
-    id SERIAL PRIMARY KEY,
-    description VARCHAR(50) NOT NULL, -- 'WAITER', 'CHEF', 'ADMIN'
-    status int,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                           id SERIAL PRIMARY KEY,
+                           tenant_id INT REFERENCES tenants(id) ON DELETE CASCADE,
+                           description VARCHAR(50) NOT NULL CHECK (description IN ('WAITER', 'CHEF', 'ADMIN')),
+                           status int default 1,
+                           created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                           UNIQUE (tenant_id, description)
 );
 
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    tenant_id INT REFERENCES tenants(id) ON DELETE CASCADE,
-    role INT REFERENCES users_role(id), -- 'WAITER', 'CHEF', 'ADMIN'
-    kitchen_id INT REFERENCES kitchens(id) ON DELETE SET NULL, -- Solo requerido para 'CHEF'
-    name VARCHAR(255) NOT NULL,
-    pin_hash VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    UNIQUE (tenant_id, name) -- Un usuario es único solo dentro de su organización (Tenant)
+                       id SERIAL PRIMARY KEY,
+                       tenant_id INT REFERENCES tenants(id) ON DELETE CASCADE,
+                       role INT REFERENCES users_role(id),
+                       kitchen_id INT REFERENCES kitchens(id) ON DELETE SET NULL, -- Solo requerido para 'CHEF'
+                       name VARCHAR(255) NOT NULL,
+                       pin_hash VARCHAR(255) NOT NULL,
+                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                       UNIQUE (tenant_id, name) -- Un usuario es único solo dentro de su organización (Tenant)
 );
 
 CREATE TABLE orders (
-    id SERIAL PRIMARY KEY,
-    tenant_id INT REFERENCES tenants(id) ON DELETE CASCADE,
-    table_id INT REFERENCES tables(id) ON DELETE SET NULL,
-    waiter_id INT REFERENCES users(id) ON DELETE SET NULL,
-    status VARCHAR(50) DEFAULT 'PENDING', -- 'PENDING', 'IN_PROGRESS', 'READY', 'DELIVERED'
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                        id SERIAL PRIMARY KEY,
+                        tenant_id INT REFERENCES tenants(id) ON DELETE CASCADE,
+                        table_id INT REFERENCES tables(id) ON DELETE SET NULL,
+                        waiter_id INT REFERENCES users(id) ON DELETE SET NULL,
+                        status VARCHAR(50) DEFAULT 'PENDING' CHECK (status IN ('PENDING', 'IN_PROGRESS', 'READY','DELIVERED')), -- 'PENDING', 'IN_PROGRESS', 'READY', 'DELIVERED'
+                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE order_items (
-     id SERIAL PRIMARY KEY,
-     order_id INT REFERENCES orders(id) ON DELETE CASCADE,
-     menu_item_id INT REFERENCES menu_items(id) ON DELETE CASCADE,
-     kitchen_id INT REFERENCES kitchens(id) ON DELETE CASCADE,
-     quantity INT NOT NULL DEFAULT 1,
-     status VARCHAR(50) DEFAULT 'PENDING', -- 'PENDING', 'READY'
-     notes TEXT,
-     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                             id SERIAL PRIMARY KEY,
+                             order_id INT REFERENCES orders(id) ON DELETE CASCADE,
+                             menu_item_id INT REFERENCES menu_items(id) ON DELETE CASCADE,
+                             kitchen_id INT REFERENCES kitchens(id) ON DELETE CASCADE,
+                             quantity INT NOT NULL DEFAULT 1,
+                             status VARCHAR(50) DEFAULT 'PENDING', -- 'PENDING', 'READY'
+                             notes TEXT,
+                             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
